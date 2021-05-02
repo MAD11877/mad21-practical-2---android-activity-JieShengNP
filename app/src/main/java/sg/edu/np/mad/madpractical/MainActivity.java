3f953bf68102cfd4fd5109fecb8c96c948773515
JieShengNP <73047869+JieShengNP@users.noreply.github.com>
2021-05-02 17:22:42 +0800

    Remove Test Codes

--- app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
@@ -8,14 +8,13 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private final String TAG = "Main Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        User user = new User("Tom", "It's Tom", 1);
+        User user = new User();
         Button follow_button = findViewById(R.id.follow_button);
         if (user.isFollowed()){
             follow_button.setText("UNFOLLOW");
@@ -26,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
         follow_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.v(TAG, "Follow Button Pressed");
                 if (user.isFollowed()){
                     follow_button.setText("FOLLOW");
                     user.setFollowed(false);
@@ -42,30 +40,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart(){
         super.onStart();
-        Log.v(TAG, "Start");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.v(TAG, "Resume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.v(TAG, "Pause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.v(TAG, "Stop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.v(TAG, "Destroy");
     }
 }
